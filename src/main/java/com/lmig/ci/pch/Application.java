b7f24ba754fc7b04cac9066f38cc9fd4d1c3534e
Trinity Gelhar <trinity.gelhar@gmail.com>
2018-02-05 15:51:13 -0500

    putting Application in package

---
+++ src/main/java/com/lmig/ci/pch/Application.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018, Liberty Mutual Group
+ *
+ * Created on Feb 5, 2018
+ */
+
+package com.lmig.ci.pch;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
